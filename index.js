a11cfa0f5a5687c6e03d83d8cd76a7d3201665a7
Denis <denismail@tut.by>
2017-10-28 03:44:37 +0300

    All middlewares accessing a memory-mapped news store have been enclosed in Express router defined in "news-store-router.js".

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-const express   = require('express');
-const NewsStore = require('./news-store.js');
+const express  = require('express');
+const nsRouter = require('./news-store-router.js');
 
 const port   = 8888;
 const server = express();
-const store  = new NewsStore({title:'test entry', summary:'automatically created for test purposes', who:'server'});
 
 
 server.use('/api/news', express.json());  // Using a built-in middleware to parse JSON body.
 
-server.get('/api/news', (req, res) => res.status(200).json(store.get('all', 'short' in req.query)));
+server.use(['/api/news/mem', '/api/news/memory'], nsRouter());    // Two distinct memory-mapped news stores
+server.use(['/api/news/mem2', '/api/news/memory2'], nsRouter());  // each mounted at two alternative routes.
 
-server.get('/api/news/:id', (req, res) => res.status(200).json(store.get(req.params.id, 'short' in req.query)));
-
-server.put('/api/news/:id', (req, res) => res.status(200).send(store.update(req.params.id, req.body)));
-
-server.post('/api/news', (req, res) => res.status(200).send(store.add(req.body)));
-
-server.delete('/api/news/:id', (req, res) => res.status(200).send(store.remove(req.params.id)));
-
-server.use((e, req, res, next) => typeof e !== 'object' || e.from !== store ? next(e) : res.status(e.code).send(e.text));
-
-server.listen(port, () => console.log(`The server is listening on port ${port}.`));
+server.listen(port, () => console.log(`Server is listening on 0.0.0.0:${port}.`));
