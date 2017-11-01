ed35e1269cbb43e363df0a364d9a2c3927abdd12
Denis <denismail@tut.by>
2017-11-01 03:35:52 +0300

    Initial implementation of POST method which adds a news entry into a MongoDB database.

--- news-mongodb-router.js
+++ news-mongodb-router.js
@@ -5,7 +5,11 @@ module.exports = (settings) => {
     const router = express.Router();
     const db = new NewsMongodb(settings);
     
-    /* Create */  router.post  ('/', (req, res) => res.status(201).send('POSTing to MongoDB is to be implemented.'));
+    // Create
+    router.post('/', (req, res) => {
+        db.add(req.body).then(m => res.status(201).send(m), e => res.status(e.code).send(e.text));
+    });
+    
     /* Read   */  router.get   ('/', (req, res) => res.status(200).send('GETing from MongoDB is to be implemented.'));
     /* Update */  router.put   ('/', (req, res) => res.status(200).send('PUTing to MongoDB is to be implemented.'));
     /* Delete */  router.delete('/', (req, res) => res.status(200).send('DELETing from MongoDB is to be implemented.'));
--- news-mongodb.js
+++ news-mongodb.js
@@ -1,21 +1,28 @@
 const mongoose = require('mongoose');
 
-const ObjectId  = mongoose.Types.ObjectId;
-const TypeObjId = mongoose.Schema.Types.ObjectId;
+const ObjectId = mongoose.Types.ObjectId;
+const objectId = mongoose.Schema.Types.ObjectId;
+
+mongoose.Promise = Promise;
 
 
 class NewsMongodb {
-    constructor () {
-        
+    constructor (settings) {
+        const c = mongoose.createConnection(`mongodb://localhost:8008/test`, {useMongoClient:true});
+        this._ModelEntry = c.model('NewsEntry', NewsMongodb.NEWS_ENTRY_SCHEMA);
+    }
+    
+    add (fields) {
+        return this._ModelEntry.create({}).then(() => 'new entry has been created', () => {throw {code:500, text:'MongoDB failed'}});
     }
 }
 
 NewsMongodb.NEWS_ENTRY_SCHEMA = new mongoose.Schema({
-    _id     : {type: TypeObjId, 'default': new ObjectId      },
-    title   : {type: String,    'default': 'no title given'  },
-    summary : {type: String,    'default': 'no summary given'},
-    body    : {type: String,    'default': 'no body given'   },
-    tags    : {type: [String],  'default': []                }
+    _id     : {type: objectId, 'default': new ObjectId      },
+    title   : {type: String,   'default': 'no title given'  },
+    summary : {type: String,   'default': 'no summary given'},
+    body    : {type: String,   'default': 'no body given'   },
+    tags    : {type: [String], 'default': []                }
 });
 
 module.exports = NewsMongodb;
