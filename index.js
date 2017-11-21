a4a590d99d7b7c0835fb2c0d017d78111569817b
Denis <denismail@tut.by>
2017-11-21 20:44:20 +0300

    Within "index.js", option {extended:false} has been given to the middleware parsing URL-encoded body contents.

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ const mongoSettings  = { port: 8008, dbName: 'newsPortal' };
 const mongoSettings2 = { port: 8008, dbName: 'newsPortal2' };
 
 
-server.use('/api/news', express.json());  // Using a built-in middleware to parse JSON body.
-server.use('/api/news', express.urlencoded());
+// Using built-in middlewares to parse requests body contents in JSON and URL-encoded formats.
+server.use('/api/news', express.json());
+server.use('/api/news', express.urlencoded({ extended: false }));
 
-server.use(['/api/news/mem', '/api/news/memory'], storeRouter());    // Two distinct memory-mapped news stores
-server.use(['/api/news/mem2', '/api/news/memory2'], storeRouter());  // each mounted at two alternative routes.
+// Two distinct memory-mapped news stores each mounted at two alternative routes.
+server.use(['/api/news/mem', '/api/news/memory'], storeRouter());
+server.use(['/api/news/mem2', '/api/news/memory2'], storeRouter());
 
-server.use('/api/news/mongo', mongoRouter(mongoSettings));    // Two distinct MongoDB news databases
-server.use('/api/news/mongo2', mongoRouter(mongoSettings2));  // each mounted at its own route.
+// Two distinct MongoDB-based news stores each mounted at its own route.
+server.use('/api/news/mongo', mongoRouter(mongoSettings));
+server.use('/api/news/mongo2', mongoRouter(mongoSettings2));
 
 server.listen(port, () => console.log(`Server is listening on 0.0.0.0:${port}.`));
