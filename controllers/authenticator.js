aec7b94f0fed9a167362b953a8eb43a03ad180c7
Denis <denismail@tut.by>
2017-11-28 00:56:23 +0300

    Module "controllers/authenticator.js" can now exercise also the Basic Authentication.

--- controllers/authenticator.js
+++ controllers/authenticator.js
@@ -2,15 +2,16 @@ const express       = require('express');
 const session       = require('express-session');
 const { Passport }  = require('passport');
 const StrategyLocal = require('passport-local').Strategy;
+const StrategyBasic = require('passport-http').BasicStrategy;
 const response      = require('../utilities/response.js');
 
 
-function local(newsStore) {
+function local(resource) {
     const expressSession = session({ secret: 'magic', saveUninitialized: false, resave: false });
     const passport = new Passport();
 
     passport.use(new StrategyLocal((username, password, done) => {
-        newsStore.authenticate(username, password).then(usr => done(null, usr), err => done(err));
+        resource.authenticate(username, password).then(user => done(null, user), err => done(err));
     }));
     passport.serializeUser((user, done) => done(null, user.id));
     passport.deserializeUser((idUser, done) => done(null, { id: idUser }));
@@ -46,4 +47,29 @@ function local(newsStore) {
     return { pass: routerPass, login: routerLogin, logout: routerLogout };
 }
 
+function basic(resource) {
+    const passport = new Passport();
+    passport.use(new StrategyBasic((username, password, done) => {
+        resource.authenticate(username, password).then(user => done(null, user), err => done(err));
+    }));
+
+    const router = express.Router();
+    router.use(passport.initialize(), (req, res, next) => {
+        passport.authenticate('basic', (error, user) => {
+            if (error || !user) {
+                res.set('WWW-Authenticate', `Basic realm="Access to ${req.baseUrl}/*"`);
+                next(error || response(401, 'No expected credentials are received.'));
+            }
+            else {
+                req.user = user;
+                next();
+            }
+        })(req, res, next);
+    });
+
+    const nextRoute = (req, res, next) => next();
+    return { pass: router, login: nextRoute, logout: nextRoute };
+}
+
 exports.local = local;
+exports.basic = basic;
